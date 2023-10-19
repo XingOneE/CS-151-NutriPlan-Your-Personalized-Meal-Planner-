8a941167c78033faa7d955cb7195136ef63c8272
MEon <104655679+XingOneE@users.noreply.github.com>
2023-10-18 20:00:51 -0700

    Update MealPlan.java

--- MealPlan.java
+++ MealPlan.java
@@ -1,42 +1,49 @@
 package package_a;
 
 public class MealPlan {
+    private FoodItem[] foods;
+    private int foodCapacity = 10;
+    private int foodCount;
+    private String dayOfWeek;
 
-    public class MealPlanner {
+    public MealPlan(String dayOfWeek) {
+        foods = new FoodItem[foodCapacity];
+        foodCount = 0;
+        this.dayOfWeek = dayOfWeek;
+    }
 
-        private MealPlan[] weeklyMealPlans;
-        private int daysInWeek = 7;
-    
-        public MealPlanner() {
-            weeklyMealPlans = new MealPlan[daysInWeek];
-            for (int i = 0; i < daysInWeek; i++) {
-                weeklyMealPlans[i] = new MealPlan();
-            }
-        }
-    
-        // Get the meal plan for a specific day of the week
-        public MealPlan getMealPlan(int day) {
-            if (day >= 1 && day <= daysInWeek) {
-                return weeklyMealPlans[day - 1]; // Subtract 1 to match array indexing
-            }
-            return null; // Invalid day
-        }
-    
-        public static void main(String[] args) {
-            // Example : 
-            // Create a meal planner for the week
-           // MealPlanner mealPlanner = new MealPlanner();
-    
-            // Example: Add a food item to the meal plan for Monday (day 1)
-            //FoodItem food1 = new FoodItem("Chicken", 250, 12, 0, 30);
-            // mealPlanner.getMealPlan(1).addFoodItem(food1);
-    
-            // Example: Display the meal plan for Monday
-            // System.out.println("Meal Plan for Monday:");
-            // mealPlanner.getMealPlan(1).displayMealPlan();
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    // Add a food item to the meal plan
+    public void addFoodItem(FoodItem food) {
+        if (foodCount < foodCapacity) {
+            foods[foodCount] = food;
+            foodCount++;
+        } else {
+            System.out.println("Meal plan is full. Cannot add more food items.");
         }
     }
 
+    // Remove a food item from the meal plan
+    public void removeFoodItem(int index) {
+        if (index >= 0 && index < foodCount) {
+            for (int i = index; i < foodCount - 1; i++) {
+                foods[i] = foods[i + 1];
+            }
+            foods[foodCount - 1] = null; // Clear the last element
+            foodCount--;
+        } else {
+            System.out.println("Invalid food item index.");
+        }
+    }
 
-    
+    // Display the contents of the meal plan
+    public void displayMealPlan() {
+        System.out.println("Meal Plan for " + dayOfWeek + ":");
+        for (int i = 0; i < foodCount; i++) {
+            System.out.println((i + 1) + ". " + foods[i].getName());
+        }
+    }
 }
