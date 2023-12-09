bbf2f76c29147d725d332fff4b887df5ab54a5a9
Kshitij Tyagi <kshitijtyagi03@gmail.com>
2023-12-08 21:06:47 -0800

    did some minor changes and commented all code

--- DataManager.java
+++ DataManager.java
@@ -1,31 +1,49 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * The DataManager class is responsible for managing data, including saving and
+ * loading Nutriplan objects to and from files.
+ */
 public class DataManager {
 
+	/**
+	 * Default constructor for the DataManager class.
+	 */
 	DataManager() {
 
 	}
 
+	/**
+	 * Saves the data from a Nutriplan object to a file.
+	 *
+	 * @param profile The Nutriplan object containing the data to be saved.
+	 * @throws FileNotFoundException If the specified file is not found.
+	 */
 	public void saveFile(Nutriplan profile) throws FileNotFoundException {
-		// C:\Users\aweso\eclipse-workspace\Nutriplan_proto
+		// Get the current working directory
 		String directory = System.getProperty("user.dir");
+
+		// Create a new File object representing a file named "NutriplanData" in the
+		// current directory
 		File newData = new File(directory + "\\NutriplanData");
 
+		// If the file doesn't exist, create a new file
 		if (!newData.exists()) {
 			try {
 				newData.createNewFile();
 			} catch (IOException e) {
-				System.out.println("An error has occured.");
+				System.out.println("An error has occurred.");
 				e.printStackTrace();
 			}
 		}
 
+		// Create a PrintWriter to write data to the file
 		PrintWriter pen = new PrintWriter(newData);
 
+		// Write personal information from the Nutriplan object to the file
 		pen.print(profile.getPersonalInfo().getName());
 		pen.print(" ");
 		pen.print(profile.getPersonalInfo().getHeight());
@@ -33,15 +51,13 @@ public class DataManager {
 		pen.print(profile.getPersonalInfo().getWeight());
 		pen.print(" ");
 
-		// allergies later
-
-		// loops through mealPlan array in MealPLanManager (total of 7)
+		// Loop through mealPlan array in MealPlanManager (total of 7)
 		for (int i = 0; i < profile.getMealPlanManager().getMealPlan().length; i++) {
-
-			// loops through foods array in MealPlan (total of 10)
+			// Loop through foods array in MealPlan (total of 10)
 			for (int j = 0; j < profile.getMealPlanManager().getMealPlan()[i].getFoods().length; j++) {
 				FoodItem food = profile.getMealPlanManager().getMealPlan()[i].getFoods()[j];
 
+				// Write food-related information to the file
 				pen.print(food.getName());
 				pen.print(" ");
 				pen.print(food.getPrice());
@@ -56,31 +72,38 @@ public class DataManager {
 				pen.print(" ");
 				pen.print(food.getSugar());
 				pen.print(" ");
-
 			}
-
 		}
 
-		System.out.println("Save sucess.");
+		// Print a success message to the console
+		System.out.println("Save success.");
 
+		// Close the PrintWriter and exit the program with a status code of 0
 		pen.close();
 		System.exit(0);
-
 	}
 
+	/**
+	 * Placeholder method for loading data from a file.
+	 */
 	public void loadFile() {
 
 	}
 
+	/**
+	 * The main method for testing the DataManager class.
+	 *
+	 * @param args Command line arguments (not used in this application).
+	 * @throws FileNotFoundException If the specified file is not found.
+	 */
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-
+		// Create a Nutriplan object for testing
 		Nutriplan test = new Nutriplan();
 
+		// Create a DataManager object
 		DataManager testSave = new DataManager();
 
+		// Save the Nutriplan object to a file
 		testSave.saveFile(test);
-
 	}
-
 }
--- MealPlannerApp.java
+++ MealPlannerApp.java
@@ -3,21 +3,35 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * The MealPlannerApp class is the main class for the Meal Planner Application.
+ */
 public class MealPlannerApp {
+    /**
+     * The main method of the application.
+     *
+     * @param args Command line arguments (not used in this application).
+     */
     public static void main(String[] args) {
-
+        // Run the GUI creation on the event dispatch thread
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
         });
     }
 
+    /**
+     * Method to create and show the GUI.
+     */
     private static void createAndShowGUI() {
+        // Create the main frame for the application
         JFrame frame = new JFrame("Meal Planner Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
+        // Create the main panel with a BorderLayout
         JPanel mainPanel = new JPanel(new BorderLayout());
 
+        // Create buttons for various options
         JButton createPlanButton = new JButton("Create Meal Plan");
         JButton getPlanButton = new JButton("Get Meal Plan for the Day");
         JButton nutritionalInfoButton = new JButton("Nutritional Information");
@@ -25,15 +39,16 @@ public class MealPlannerApp {
         JButton setGoalButton = new JButton("Set Goal for Meal Plan");
         JButton exitButton = new JButton("Exit");
 
+        // ActionListener for the "Create Meal Plan" option
         createPlanButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Logic for the "Create Meal Plan" option
                 JOptionPane.showMessageDialog(frame, "Create Meal Plan Option Selected");
-
             }
         });
 
+        // ActionListener for the "Get Meal Plan for the Day" option
         getPlanButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -42,6 +57,7 @@ public class MealPlannerApp {
             }
         });
 
+        // ActionListener for the "Nutritional Information" option
         nutritionalInfoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -50,6 +66,7 @@ public class MealPlannerApp {
             }
         });
 
+        // ActionListener for the "View/Edit Meal Plan" option
         viewEditPlanButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,6 +75,7 @@ public class MealPlannerApp {
             }
         });
 
+        // ActionListener for the "Set Goal for Meal Plan" option
         setGoalButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -66,6 +84,7 @@ public class MealPlannerApp {
             }
         });
 
+        // ActionListener for the "Exit" option
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -74,18 +93,20 @@ public class MealPlannerApp {
             }
         });
 
+        // Add buttons to the main panel in BorderLayout positions
         mainPanel.add(createPlanButton, BorderLayout.NORTH);
         mainPanel.add(getPlanButton, BorderLayout.CENTER);
         mainPanel.add(nutritionalInfoButton, BorderLayout.SOUTH);
 
+        // Create a side panel with buttons and add it to the main panel
         JPanel sidePanel = new JPanel();
         sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
         sidePanel.add(viewEditPlanButton);
         sidePanel.add(setGoalButton);
         sidePanel.add(exitButton);
-
         mainPanel.add(sidePanel, BorderLayout.EAST);
 
+        // Add the main panel to the frame and make it visible
         frame.add(mainPanel);
         frame.setVisible(true);
     }
